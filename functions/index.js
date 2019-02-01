506e58383bb3a1d4d8b947cffc919a773fe3a3a8
t-kojima <t-kojima@sonicgarden.jp>
2019-02-01 21:19:32 +0900

    chore: firestore settings

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-exports.insertData = functions.https.onRequest((request, response) => {
-  response.header({
+admin.initializeApp();
+const firestore = admin.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+const usersRef = firestore.collection('users');
+process.on('unhandledRejection', console.dir);
+
+exports.insertData = functions.https.onRequest(async ({ method, headers: { 'firebase-user-token': token } }, res) => {
+  res.header({
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Credentials':  true,
+    'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Methods': '*',
   });
-  response.send("hello");
+  if(['OPTIONS'].includes(method)) return res.send('');
+  const { uid } = await admin.auth().verifyIdToken(token);
+  const userSnap = await usersRef.doc(uid).get();
+  const allowAccess = userSnap.data() && userSnap.data().allowAccess;
+  res.send(!!allowAccess);
 });
